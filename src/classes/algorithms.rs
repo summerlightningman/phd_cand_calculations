8fc81271d067a306a1bfaf206ae13df1becbc7cf
Dimitriy White <oblivvion333@gmail.com>
2024-11-06 22:50:25 +0300

    Снизил численность алгоритмов для прогонов

--- src/classes/algorithms.rs
+++ src/classes/algorithms.rs
@@ -1,49 +1,37 @@
 use super::algorithm_params::AlgorithmParams;
 
-pub const ALGORITHMS: [AlgorithmParams; 51] = [
+pub const ALGORITHMS: [AlgorithmParams; 24] = [
     // AC variants
     AlgorithmParams::AC {
         alpha: 1.25,
         beta: 0.5,
         q: 0.75,
         p: 1.0,
-    },
+    }, //
     AlgorithmParams::AC {
         alpha: 0.75,
         beta: 1.0,
         q: 1.25,
         p: 0.5,
-    },
+    }, //
     AlgorithmParams::AC {
         alpha: 1.5,
         beta: 0.5,
         q: 0.75,
         p: 0.5,
-    },
+    }, //
     AlgorithmParams::AC {
         alpha: 1.25,
         beta: 0.5,
         q: 1.0,
         p: 0.75,
-    },
-    AlgorithmParams::AC {
-        alpha: 0.5,
-        beta: 0.5,
-        q: 1.0,
-        p: 0.75,
-    },
-    AlgorithmParams::AC {
-        alpha: 0.25,
-        beta: 0.5,
-        q: 1.0,
-        p: 0.75,
-    },
+    }, //
     AlgorithmParams::AC {
         alpha: 1.5,
         beta: 0.5,
         q: 1.0,
         p: 0.75,
-    },
+    }, //
     AlgorithmParams::AC {
         alpha: 1.5,
         beta: 0.75,
@@ -55,42 +43,20 @@ pub const ALGORITHMS: [AlgorithmParams; 51] = [
         beta: 1.5,
         q: 1.25,
         p: 1.25,
-    },
+    }, //
     AlgorithmParams::AC {
         alpha: 1.5,
         beta: 1.25,
         q: 1.0,
         p: 0.75,
-    },
-    AlgorithmParams::AC {
-        alpha: 0.25,
-        beta: 1.0,
-        q: 1.0,
-        p: 0.75,
-    },
+    }, //
     // BC variants
     AlgorithmParams::BC {
         workers_part: 0.15,
-        regenerate_func: "swap_indexes",
-    },
-    AlgorithmParams::BC {
-        workers_part: 0.15,
         regenerate_func: "reverse_elements",
     },
     AlgorithmParams::BC {
         workers_part: 0.25,
-        regenerate_func: "swap_indexes",
-    },
-    AlgorithmParams::BC {
-        workers_part: 0.25,
-        regenerate_func: "reverse_elements",
-    },
-    AlgorithmParams::BC {
-        workers_part: 0.4,
-        regenerate_func: "swap_indexes",
-    },
-    AlgorithmParams::BC {
-        workers_part: 0.4,
         regenerate_func: "reverse_elements",
     },
     AlgorithmParams::BC {
@@ -98,20 +64,8 @@ pub const ALGORITHMS: [AlgorithmParams; 51] = [
         regenerate_func: "swap_indexes",
     },
     AlgorithmParams::BC {
-        workers_part: 0.5,
-        regenerate_func: "reverse_elements",
-    },
-    AlgorithmParams::BC {
-        workers_part: 0.6,
-        regenerate_func: "swap_indexes",
-    },
-    AlgorithmParams::BC {
         workers_part: 0.6,
         regenerate_func: "reverse_elements",
-    },
-    AlgorithmParams::BC {
-        workers_part: 0.65,
-        regenerate_func: "swap_indexes",
     },
     AlgorithmParams::BC {
         workers_part: 0.65,
@@ -124,10 +78,6 @@ pub const ALGORITHMS: [AlgorithmParams; 51] = [
     AlgorithmParams::BC {
         workers_part: 0.75,
         regenerate_func: "reverse_elements",
-    },
-    AlgorithmParams::BC {
-        workers_part: 0.9,
-        regenerate_func: "swap_indexes",
     },
     AlgorithmParams::BC {
         workers_part: 0.9,
@@ -137,13 +87,8 @@ pub const ALGORITHMS: [AlgorithmParams; 51] = [
     AlgorithmParams::GA {
         p_mutation: 0.2,
         select_func: "roulette",
-        mutate_func: "swap_indexes",
-    },
-    AlgorithmParams::GA {
-        p_mutation: 0.2,
-        select_func: "roulette",
         mutate_func: "reverse_elements",
-    },
+    }, //
     AlgorithmParams::GA {
         p_mutation: 0.2,
         select_func: "stochastic",
@@ -157,101 +102,26 @@ pub const ALGORITHMS: [AlgorithmParams; 51] = [
     AlgorithmParams::GA {
         p_mutation: 0.2,
         select_func: "tournament",
-        mutate_func: "swap_indexes",
-    },
-    AlgorithmParams::GA {
-        p_mutation: 0.2,
-        select_func: "tournament",
         mutate_func: "reverse_elements",
     },
     AlgorithmParams::GA {
         p_mutation: 0.4,
-        select_func: "roulette",
-        mutate_func: "swap_indexes",
-    },
-    AlgorithmParams::GA {
-        p_mutation: 0.4,
-        select_func: "roulette",
-        mutate_func: "reverse_elements",
-    },
-    AlgorithmParams::GA {
-        p_mutation: 0.4,
-        select_func: "stochastic",
-        mutate_func: "reverse_elements",
-    },
-    AlgorithmParams::GA {
-        p_mutation: 0.4,
-        select_func: "stochastic",
-        mutate_func: "swap_indexes",
-    },
-    AlgorithmParams::GA {
-        p_mutation: 0.4,
         select_func: "tournament",
         mutate_func: "swap_indexes",
-    },
+    }, //
     AlgorithmParams::GA {
         p_mutation: 0.4,
         select_func: "tournament",
         mutate_func: "reverse_elements",
-    },
-    AlgorithmParams::GA {
-        p_mutation: 0.6,
-        select_func: "roulette",
-        mutate_func: "swap_indexes",
-    },
-    AlgorithmParams::GA {
-        p_mutation: 0.6,
-        select_func: "roulette",
-        mutate_func: "reverse_elements",
-    },
-    AlgorithmParams::GA {
-        p_mutation: 0.6,
-        select_func: "stochastic",
-        mutate_func: "reverse_elements",
-    },
-    AlgorithmParams::GA {
-        p_mutation: 0.6,
-        select_func: "stochastic",
-        mutate_func: "swap_indexes",
-    },
+    }, //
     AlgorithmParams::GA {
         p_mutation: 0.6,
         select_func: "tournament",
         mutate_func: "swap_indexes",
-    },
+    }, //
     AlgorithmParams::GA {
         p_mutation: 0.6,
         select_func: "tournament",
         mutate_func: "reverse_elements",
-    },
-    AlgorithmParams::GA {
-        p_mutation: 0.8,
-        select_func: "roulette",
-        mutate_func: "swap_indexes",
-    },
-    AlgorithmParams::GA {
-        p_mutation: 0.8,
-        select_func: "roulette",
-        mutate_func: "reverse_elements",
-    },
-    AlgorithmParams::GA {
-        p_mutation: 0.8,
-        select_func: "stochastic",
-        mutate_func: "reverse_elements",
-    },
-    AlgorithmParams::GA {
-        p_mutation: 0.8,
-        select_func: "stochastic",
-        mutate_func: "swap_indexes",
-    },
-    AlgorithmParams::GA {
-        p_mutation: 0.8,
-        select_func: "tournament",
-        mutate_func: "swap_indexes",
-    },
-    AlgorithmParams::GA {
-        p_mutation: 0.8,
-        select_func: "tournament",
-        mutate_func: "reverse_elements",
-    },
+    }, //
 ];
